6dacd8a86235fbb3dda5051b07349b3600402713
Roman Shcherbakov <daredever@gmail.com>
2020-08-09 09:58:30 +0300

    prepare solution, add text editor project #1, add project for templates #8

---
+++ src/Templates/InterfaceTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
