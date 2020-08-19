a48c6883a41184cb29fc50a8b5d1d7264fc59f45
Roman Shcherbakov <daredever@gmail.com>
2020-08-19 12:15:27 +0300

    tool options refactoring #13

--- src/Yae.Tool/Program.cs
+++ src/Yae.Tool/Program.cs
@@ -1,30 +1,31 @@
-﻿using System.IO;
-using McMaster.Extensions.CommandLineUtils;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CommandLine;
 using Yae.Core;
 
 namespace Yae.Tool
 {
     public sealed class Program
     {
-        public static int Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            var app = new CommandLineApplication();
-            app.HelpOption("-h|--help");
-            app.VersionOption("-v|--version", "0.0.1");
+            var result = Parser.Default.ParseArguments<Options>(args);
+            await result.WithParsedAsync(options => RunEditorAsync(options));
+        }
 
-            var optionFile = app.Option("-f|--file <FILE>", "File", CommandOptionType.SingleValue);
-            var optionLinesPerPage = app.Option<int>("-n|--count <N>", "Lines per page", CommandOptionType.SingleValue);
-
-            app.OnExecuteAsync(async cancellationToken =>
+        private static async Task RunEditorAsync(Options options)
+        {
+            try
             {
-                var path = optionFile.HasValue() ? optionFile.Value() : string.Empty;
-                var linesPerPage = optionLinesPerPage.HasValue() ? optionLinesPerPage.ParsedValue : 30;
-                var fileInfo = new FileInfo(path);
-                var textEditor = new TextEditor(fileInfo, linesPerPage);
+                var file = new FileInfo(options.File);
+                var textEditor = new TextEditor(file, options.LinesCount);
                 await textEditor.RunAsync();
-            });
-
-            return app.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
